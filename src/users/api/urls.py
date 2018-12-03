71269b22678c355f17fe9aa44b4e0d6b2cb1ffbb
Paweł Drapiewski <drapek39@wp.pl>
2018-12-04 00:25:15 +0100

    Add tests for reset-password endpoint.

--- src/users/api/urls.py
+++ src/users/api/urls.py
@@ -3,6 +3,6 @@ from rest_framework.authtoken import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('token-auth/', auth_views.obtain_auth_token, 'get_token'),
-    path('reset-passwd/', views.ChangePasswordView.as_view(), 'reset_passwd')
+    path('token-auth/', auth_views.obtain_auth_token, name='get-token'),
+    path('reset-passwd/', views.ChangePasswordView.as_view(), name='reset-passwd')
 ]
--- src/users/tests.py
+++ src/users/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 
-# Create your tests here.
 from rest_framework.test import APIClient
 
 from users.models import User
@@ -9,18 +8,38 @@ from django.urls import reverse
 
 class UserAuthenticationTestCase(TestCase):
     def setUp(self):
-        test_user = User(username="tester1", last_name="Test")
-        test_user.set_password("test1")
+        self.test_user = User(username="tester1", last_name="Test")
+        self.test_user.set_password("test1")
+        self.test_user.save()
 
         self.api_client = APIClient()
 
-    def test_token_generation_bad_passwd(self):
+    def test_token_generation_bad_password(self):
         data = {"username": "tester1", "password": "bad_one"}
-        response = self.api_client.post(reverse('get_token'), data=data)
-        assert response.status_code == 404
+        response = self.api_client.post(reverse('get-token'), data=data, format='json')
+        self.assertEqual(response.status_code, 400)
 
     def test_token_generation(self):
         data = {"username": "tester1", "password": "test1"}
-        response = self.api_client.post('get_token', data=data)
-        assert response.status_code == 200
-        print(response.data)
+        response = self.api_client.post(reverse('get-token'), data=data, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('token' in response.data)
+
+    def test_password_change_bad_password(self):
+        data = {"old_password": "bad", "new_password": "asdasd123"}
+        self.api_client.force_authenticate(user=self.test_user)
+        response = self.api_client.put(reverse('reset-passwd'), data=data, format='json')
+        self.api_client.force_authenticate(token=None)
+        self.assertEqual(response.status_code, 400)
+
+    def test_password_change_good(self):
+        data = {"old_password": "test1", "new_password": "asdasd123"}
+        self.api_client.force_authenticate(user=self.test_user)
+        response = self.api_client.put(reverse('reset-passwd'), data=data, format='json')
+        self.api_client.force_authenticate(token=None)
+        self.assertEqual(response.status_code, 200)
+
+    def test_password_not_authorized(self):
+        data = {"old_password": "test1", "new_password": "asdasd123"}
+        response = self.api_client.put(reverse('reset-passwd'), data=data, format='json')
+        self.assertEqual(response.status_code, 401)
