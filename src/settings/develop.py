9743682139e729c21099e889c806753d70eeed9d
Pawel Drapiewski <drapek39@gmail.com>
2019-04-07 19:49:22 +0200

    Create docker files for develop environment.
    * Refactor file structure - split requirements, create docker folder.
    * Create docker-compose config file
    * Create Dockerfile for web (django) service.

--- src/settings/develop.py
+++ src/settings/develop.py
@@ -4,15 +4,18 @@
 
 from .base import *
 
-
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DEBUG = True
 
 DATABASES = {
+    # By default it is run as docker image
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
